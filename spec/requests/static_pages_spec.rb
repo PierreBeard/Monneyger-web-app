1a2f15175af798aa5ea89fc0281278bd953b246c
PierreBeard <beardpierre@gmail.com>
2013-11-11 19:28:42 +0100

    USer page create

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,19 +4,26 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+    it "should have the content 'Moneyger'" do
+      visit root_path
+      expect(page).to have_content('Moneyger')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+      visit root_path
+      expect(page).to have_title("MonneyGer App Website")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+  end  
 end
